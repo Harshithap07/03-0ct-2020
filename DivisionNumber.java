5fcca62f57c2d6f8701722136bc004bc60532893
Harshitha P <73736464+Harshithap07@users.noreply.github.com>
2020-11-03 11:54:54 +0530

    Add files via upload

---
+++ DivisionNumber.java
@@ -0,0 +1,10 @@
+class DivisionNumber{
+public static void main (String args[]){
+int num[]={3,4,5,6,7,8,9,10,11,12};
+for(int i=0;i<12;i++){
+if(num[i]%2==0){
+System.out.println(num[i]);
+}
+}
+}
+}
